9a3c2238c8f1e616d2a3232b8763f587303a0123
Georgiy <georgiy.pashin@yandex.ru>
2023-11-03 10:42:32 +0300

    не проходит один тест

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,32 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    if (arr1.length !== arr2.length) {
+return false
+    }
+   const compare =  arr1.every((element, index) => element === arr2[index]);
+return compare
+     }
+// вторая задача
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+function getSumOfAges (total, age){
+    return total + age
+ }
+const initialValue = 0;
+const curientGender = users.filter(user => user.gender === gender);
+const curientUsersAge = curientGender.map(user => user.age);
+
+const subTotal = curientUsersAge.reduce(getSumOfAges,initialValue)
+
+if(users === [] && gender === "мужской") {
+    return 0
 }
+if (gender !== "мужской" && gender !== "женский" ) {
+    return 0
+}
+
+let result = subTotal / curientUsersAge.length;
+return result
+}
+
+//let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((accumulator, user) => accumulator + currentValue, initialValue)
